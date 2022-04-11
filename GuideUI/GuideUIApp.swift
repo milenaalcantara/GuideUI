c434f8e954f1f47a5ecb60def04292add02d8d6b
Milena Alcantara <milena.lima.alcantara61@aluno.ifce.edu.br>
2022-04-11 14:09:06 -0300

    first commit

---
+++ GuideUI/GuideUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuideUIApp.swift
+//  GuideUI
+//
+//  Created by Milena Lima de Alcântara on 11/04/22.
+//
+
+import SwiftUI
+
+@main
+struct GuideUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
